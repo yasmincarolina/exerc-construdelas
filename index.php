c0dc271cc476935ac433f2fbc81d481612b86c64
yasmincarolina <91809784+yasmincarolina@users.noreply.github.com>
2021-10-03 19:31:46 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendendo básico de CSS</title>
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+  </head>
+  <body class="tela-azul">
+    Taananananaaa
+  </body>
+</html>
